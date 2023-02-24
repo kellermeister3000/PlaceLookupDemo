6b8748341255c6f6d42c3f6fa5761e2422fe2f0c
Philip Keller <philipjoelkeller@gmail.com>
2023-02-23 21:55:02 -0800

    Location Manager finding Device's Longitude and Latitude - YouTube 8.7

--- PlaceLookupDemo/PlaceLookupDemoApp.swift
+++ PlaceLookupDemo/PlaceLookupDemoApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct PlaceLookupDemoApp: App {
+    @StateObject var locationManager = LocationManager()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(locationManager)
         }
     }
 }
