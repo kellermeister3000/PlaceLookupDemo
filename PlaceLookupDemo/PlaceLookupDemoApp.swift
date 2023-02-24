5cc29d658d41400917780e93a03aca2e98bba722
Philip Keller <philipjoelkeller@gmail.com>
2023-02-23 21:23:52 -0800

    Initial Commit

---
+++ PlaceLookupDemo/PlaceLookupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceLookupDemoApp.swift
+//  PlaceLookupDemo
+//
+//  Created by Philip Keller on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaceLookupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
